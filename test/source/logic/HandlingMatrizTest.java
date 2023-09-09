ffca9c7c4996a935af8538fc9ef1c71d31531212
Jairo Armando <jairo.armando@gmail.com>
2023-09-09 18:36:18 -0500

    Version con la implementacion de los metodos de suma de filas, columnas y multiplicacion de natrices. Tambien se agrega las pruebas unitarias

---
+++ test/source/logic/HandlingMatrizTest.java
@@ -0,0 +1,44 @@
+package logic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HandlingMatrizTest {
+
+    @Test
+    void isMagic() {
+    }
+
+    @Test
+    void sumRows() {
+    }
+
+    @Test
+    void sumCol() {
+    }
+
+    @Test
+    void getMainDiag() {
+    }
+
+    @Test
+    void getSecDiag() {
+    }
+
+    @Test
+    void multMatriz() {
+    }
+
+    @Test
+    void getTranspuesta() {
+    }
+
+    @Test
+    void sumMatriz() {
+    }
+
+    @Test
+    void getInversa() {
+    }
+}
